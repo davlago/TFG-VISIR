15645d8b462463838250f97a5cd5fd005ce24fc1
davlago <davlago@ucm.es>
2022-12-06 12:24:20 +0100

    comenzando v1.0.0.1

--- src/myScene.js
+++ src/myScene.js
@@ -7,13 +7,14 @@
  import * as THREE from 'three';
 
 let container = document.getElementById( 'threejs' );
+let scene;
 let renderer;
 
 export default class myScene extends Entity {
     constructor() {
         super();
-        this.scene = new THREE.Scene();
         this.camera = new Camera();
+        scene = new THREE.Scene();
         renderer = new THREE.WebGLRenderer({
             canvas: this.canvas
         });
