a3815b98f2f857312a4ca55c1efbfa9fc24fd010
davlago <79446614+davlago@users.noreply.github.com>
2022-12-25 02:20:44 -0500

    Update modelManager.js

--- src/modelManager.js
+++ src/modelManager.js
@@ -4,75 +4,45 @@
 
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 
-let young;
-let adult;
-let elderly;
+
 export default class ModelManager {
 
     constructor() {
+        this.models={};
     }
 
     loadModels(modelsInfoArray) {
         return new Promise((resolve, reject) => {
-            loadYoung().then(function () {
-                loadAdult().then(function () {
-                    loadElderly().then(function () {
-                        resolve();
-                    });
-                });
+            var promises=[];
+            for (var i in modelsInfoArray) {
+                promises.push(this.load(modelsInfoArray[i]))
+            }
+
+            Promise.all(promises).then(function(){
+                console.log("All models loaded");
+                resolve();
             });
+
 
         });
     }
 
-    getModelsArray() {
-        return [young, adult, elderly]
+
+
+    load(model){
+        return new Promise((resolve, reject) => {
+            let fbxLoader = new FBXLoader();
+            fbxLoader.load(model.file,
+                (object) => {
+                    object.scale.set(0.06, 0.06, 0.06);
+                    this.models[model.key]=object;
+                    console.log("Loaded "+model.key);
+                    resolve();
+                });
+        });
     }
+    
 }
 
-async function loadYoung() {
-    return new Promise((resolve, reject) => {
-        let fbxLoader = new FBXLoader();
-        fbxLoader.load(
-            '/models/young.fbx',
-            (object) => {
-                young = object;
-                young.scale.set(0.06, 0.06, 0.06);
-                console.log("Cargado young")
-                resolve();
-            });
 
-    });
-
-}
-
-async function loadAdult() {
-    return new Promise((resolve, reject) => {
-        let fbxLoader = new FBXLoader();
-        fbxLoader.load(
-            '/models/adult.fbx',
-            (object) => {
-                adult = object;
-                adult.scale.set(0.07, 0.07, 0.07);
-                console.log("Cargado adult")
-                resolve();
-            });
-
-    });
-}
-
-async function loadElderly() {
-    return new Promise((resolve, reject) => {
-        let fbxLoader = new FBXLoader();
-        fbxLoader.load(
-            '/models/elderly.fbx',
-            (object) => {
-                elderly = object;
-                elderly.scale.set(0.06, 0.06, 0.06);
-                console.log("Cargado elderly")
-                resolve();
-            });
-
-    });
-}
 
