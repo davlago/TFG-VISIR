91fc89a82b35cf92b014879e12e5deed35e760a4
David Lago <david.lago@calimaco.com>
2023-04-28 10:40:40 +0200

    testgh

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -15,8 +15,7 @@ module.exports = {
         [
             new CopyWebpackPlugin({
                 patterns: [
-                    { from: path.resolve(__dirname, '../assets') }
-                ]
+                    { from: path.resolve(__dirname, '../assets'), to: path.resolve(__dirname, '../dist/assets')}                ]
             }),
             new HtmlWebpackPlugin({
                 template: path.resolve(__dirname, '../index.html'),
